c4b9915299fed7475c61af1db1c5df83553d4b6b
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-06-18 18:26:54 +0700

    added relation to transactions table

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,6 +26,8 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'username',
+        'phone',
         'password',
     ];
 
@@ -58,4 +60,9 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class, 'users_id', 'id');
+    }
 }
