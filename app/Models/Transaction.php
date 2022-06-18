2191459ea8b3f2744765e9e959e6ad6c31b0dea0
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-06-18 20:00:05 +0700

    created transaction model for handling transaction table and relation to user and transaction item table

---
+++ app/Models/Transaction.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use App\Models\TransactionItem;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Transaction extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $guarded = ['id'];
+    protected $fillable = ['users_id', 'address', 'payment', 'shipping_price', 'total_price', 'status'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'users_id', 'id');
+    }
+
+    public function items()
+    {
+        return $this->hasMany(TransactionItem::class, 'transactions_id', 'id');
+    }
+}
