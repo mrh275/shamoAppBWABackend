e9e9ab3f2e1442673ff83c4d227d8de3b49dee27
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-06-18 19:58:21 +0700

    created model for relation to product category and product gallery

---
+++ app/Models/Product.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\ProductGallery;
+use App\Models\ProductCategory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Product extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $guarded = ['id'];
+    protected $fillable = ['name', 'description', 'price', 'categories_id', 'tags'];
+
+    public function galleries()
+    {
+        return $this->hasMany(ProductGallery::class, 'products_id', 'id');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(ProductCategory::class, 'categories_id', 'id');
+    }
+}
