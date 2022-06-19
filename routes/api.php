013db05311fbd80ff37ef696f9f4b811f0adc03a
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-06-19 22:15:11 +0700

    created route for transaction info

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Http\Controllers\API\ProductCategoryController;
-use App\Http\Controllers\API\ProductController;
-use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\API\ProductController;
+use App\Http\Controllers\API\TransactionController;
+use App\Http\Controllers\API\ProductCategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,4 +28,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [UserController::class, 'fetch']);
     Route::post('/user', [UserController::class, 'updateProfile']);
     Route::post('/logout', [UserController::class, 'logout']);
+
+    Route::get('/transactions', [TransactionController::class, 'all']);
 });
