3bfb301ac0855a53941362c48772561c278b030d
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-06-19 22:15:28 +0700

    created transaction controller for handling transaction info

---
+++ app/Http/Controllers/API/TransactionController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Transaction;
+use Illuminate\Http\Request;
+use App\Helpers\ResponseFormatter;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class TransactionController extends Controller
+{
+    public function all(Request $request)
+    {
+        $id = $request->input('id');
+        $limit = $request->input('limit', 6);
+        $status = $request->input('status');
+
+        if ($id) {
+            $transaction = Transaction::with(['items.product'])->find($id);
+            if ($transaction) {
+                return ResponseFormatter::success(
+                    $transaction,
+                    'Transaksi ditemukan',
+                );
+            } else {
+                return ResponseFormatter::error(
+                    null,
+                    'Transaksi tidak ditemukan',
+                    404
+                );
+            }
+        }
+
+        $transaction = Transaction::with(['items.product'])->where('users_id', Auth::user()->id);
+
+        if ($status) {
+            $transaction->where('status', $status);
+        }
+
+        return ResponseFormatter::success(
+            $transaction->paginate($limit),
+            'Transaksi ditemukan',
+        );
+    }
+}
