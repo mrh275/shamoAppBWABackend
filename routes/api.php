9fca86124aa5a958b33fe36f87f1f55ebd0daa7d
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-06-19 21:51:59 +0700

    created route for get data profile

--- routes/api.php
+++ routes/api.php
@@ -17,12 +17,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::get('/products', [ProductController::class, 'all']);
 Route::get('/categories', [ProductCategoryController::class, 'all']);
 
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [UserController::class, 'fetch']);
+});
