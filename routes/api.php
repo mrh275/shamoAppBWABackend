81eb469a13e13448f6ac3a069ac090fec97d998a
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-06-19 21:52:20 +0700

    created route for update data profile

--- routes/api.php
+++ routes/api.php
@@ -25,4 +25,5 @@ Route::post('/login', [UserController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [UserController::class, 'fetch']);
+    Route::post('/user', [UserController::class, 'updateProfile']);
 });
